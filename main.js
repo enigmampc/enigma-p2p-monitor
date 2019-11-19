0ac6f19f20fb562fbcf79b3ecbbcde1867494d2f
assafmo <assaf.morami@gmail.com>
2019-11-19 10:40:26 +0200

    refactor with promisify

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
-const [boostrapNodeAddress] = process.argv.slice(process.argv.length - 1);
+const boostrapNodes = process.argv.slice(2);
 
-console.log("boostrap:", boostrapNodeAddress);
+boostrapNodes.forEach(n => {
+  console.log("boostrap\t" + n);
+});
 
 const TCP = require("libp2p-tcp");
 const Mplex = require("libp2p-mplex");
 const KadDHT = require("libp2p-kad-dht");
 const Bootstrap = require("libp2p-bootstrap");
+const SECIO = require("libp2p-secio");
 const SPDY = require("libp2p-spdy");
 const WS = require("libp2p-websockets");
 const PeerInfo = require("peer-info");
 // const Gossipsub = require("libp2p-gossipsub");
 // const FloodSub = require("libp2p-floodsub");
 const defaultsDeep = require("@nodeutils/defaults-deep");
+
+const { promisify } = require("util");
 
 const libp2p = require("libp2p");
 
@@ -23,6 +28,7 @@ class MyBundle extends libp2p {
         transport: [TCP, WS],
         streamMuxer: [Mplex, SPDY],
         peerDiscovery: [Bootstrap],
+        // connEncryption: [SECIO],
         dht: KadDHT
       },
       config: {
@@ -34,10 +40,11 @@ class MyBundle extends libp2p {
           pubsub: true
         },
         peerDiscovery: {
+          // autoDial: true, // TODO check
           bootstrap: {
             interval: 2000,
             enabled: true,
-            list: [boostrapNodeAddress]
+            list: boostrapNodes
           }
         }
       }
@@ -47,67 +54,44 @@ class MyBundle extends libp2p {
   }
 }
 
-PeerInfo.create((err, peerInfo) => {
-  if (err) {
-    throw err;
-  }
+(async () => {
+  const PeerInfoCreate = promisify(PeerInfo.create).bind(PeerInfo);
+  const peerInfo = await PeerInfoCreate();
+  // TODO catch PeerInfoCreate
 
   peerInfo.multiaddrs.add("/ip4/0.0.0.0/tcp/0");
 
-  const node = new MyBundle({
-    peerInfo,
-    config: {
-      peerDiscovery: {
-        bootstrap: {
-          enabled: true,
-          list: [boostrapNodeAddress]
-        }
-      }
-    }
+  const node = new MyBundle({ peerInfo });
+
+  const nodeStart = promisify(node.start).bind(node);
+  await nodeStart();
+  // No need to try/catch. Let it throw.
+
+  console.log(`my_libp2p_id\t${node.peerInfo.id.toB58String()}`);
+
+  node.on("peer:discovery", async peerInfo => {
+    const nodeDial = promisify(node.dial).bind(node);
+    await nodeDial(peerInfo);
+    // No need to try/catch. Let it throw.
   });
 
-  node.start(err => {
-    if (err) {
-      throw err;
-    }
-    console.log("my id:", node.peerInfo.id.toB58String());
+  node.on("peer:connect", peerInfo => {
+    console.log("peer:connect\t" + peerInfo.id.toB58String());
 
-    node.once("peer:discovery", peerInfo => {
-      console.log("peer:discovery", peerInfo.id.toB58String());
-      node.dial(peerInfo, (err, conn) => {
-        if (err) {
-          throw err;
-        }
-      });
-    });
+    node.pubsub.subscribe(
+      "/taskresults/0.1",
+      msg => {
+        console.log("/taskresults/0.1", msg.from, msg.data.toString());
+      },
+      () => {}
+    );
 
-    node.on("peer:connect", peerInfo => {
-      console.log("peer:connect", peerInfo.id.toB58String());
-
-      node.pubsub.subscribe(
-        "/taskresults/0.1",
-        msg => {
-          console.log("/taskresults/0.1", msg);
-        },
-        () => {}
-      );
-
-      node.pubsub.subscribe(
-        "/broadcast/0.1",
-        msg => {
-          console.log("/broadcast/0.1", msg.from, msg.data.toString());
-        },
-        () => {}
-      );
-
-      node.pubsub.publish("/broadcast/0.1", Buffer.from("some msg"), err => {
-        if (err) {
-          console.log("erro publish /broadcast/0.1", err);
-          return;
-        }
-
-        console.log("publish /broadcast/0.1");
-      });
-    });
+    node.pubsub.subscribe(
+      "/broadcast/0.1",
+      msg => {
+        console.log("/broadcast/0.1", msg.from, msg.data.toString());
+      },
+      () => {}
+    );
   });
-});
+})();
