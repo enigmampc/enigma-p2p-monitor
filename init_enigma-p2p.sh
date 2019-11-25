274669021641ed46793cf64344a09a3b81655603
assafmo <assaf.morami@gmail.com>
2019-11-25 10:48:32 +0200

    init_enigma-p2p.sh

---
+++ init_enigma-p2p.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -f "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json" ]]; then
+    exit
+fi
+
+rm -rf /tmp/enigma-p2p
+git clone -b develop --single-branch https://github.com/enigmampc/enigma-p2p.git /tmp/enigma-p2p
+cd /tmp/enigma-p2p
+npm install
+
+npm install truffle
+cd test/ethereum/scripts
+rm -rf build
+npx truffle compile
