a1ecbb0e8476ad5cc5912e0d65a85e1aa3d82d88
assafmo <assaf.morami@gmail.com>
2019-11-25 12:15:07 +0200

    tests are passing

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-let { spawn, ChildProcess } = require("child_process");
+const { spawn } = require("child_process");
 const exec = require("util").promisify(require("child_process").exec);
 const fs = require("fs");
+const assert = require("assert");
 
 let truffleDevelopProcess;
 let account;
 
-beforeAll(async () => {
+before(async function() {
+  this.timeout(120000);
+
   if (!fs.existsSync("/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json")) {
     await exec(`rm -rf /tmp/enigma-p2p`);
     await exec(`git clone -b develop --single-branch https://github.com/enigmampc/enigma-p2p.git /tmp/enigma-p2p`);
@@ -20,29 +23,83 @@ beforeAll(async () => {
       data = data.toString().match(/\(0\) 0x[a-f0-9]+/);
       if (Array.isArray(data) && data.length === 1) {
         account = data[0].split(" ")[1];
+        assert(account.length > 0);
         resolve();
       }
     });
   });
-}, 120000);
+});
+
+after(async () => {
+  await exec(`kill -KILL ${truffleDevelopProcess.pid}`);
+});
 
 let enigmaContractAddress;
-beforeEach(async () => {
-  enigmaContractAddress = (await exec(`npx truffle migrate`, { cwd: "/tmp/enigma-p2p/test/ethereum/scripts" })).stdout
+beforeEach(async function() {
+  this.timeout(60000);
+
+  enigmaContractAddress = (
+    await exec(`npx truffle migrate --reset`, { cwd: "/tmp/enigma-p2p/test/ethereum/scripts" })
+  ).stdout
     .trim()
     .match(/contract address:\s+0x[a-fA-f0-9]+/g)
     .slice(-1)[0]
     .split(/\s/)
     .slice(-1)[0];
-}, 60000);
 
-afterAll(async () => {
-  await exec(`kill -KILL ${truffleDevelopProcess.pid}`);
+  assert(enigmaContractAddress.length > 0);
 });
 
-test("connect to bootstrap", () => {
-  return;
+let killList = [];
+beforeEach(async () => {
+  killList = [];
+});
+
+afterEach(async () => {
+  for (const process of killList) {
+    try {
+      await exec(`kill -KILL ${process.pid}`);
+    } catch (e) {}
+  }
+  killList = [];
+});
+
+it("subscribe to /broadcast/0.1 and /taskresult/0.1", function() {
+  this.timeout(60000);
+
+  const monitor = spawn("node", [
+    "main.js",
+    "--bootstrap",
+    "/ip4/127.0.0.1/tcp/10300/ipfs/QmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm",
+    "--enigma-contract-json-path",
+    "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json",
+    "--enigma-contract-address",
+    enigmaContractAddress
+  ]);
+  killList.push(monitor);
+
   return new Promise(resolve => {
+    let broadcast = false;
+    let taskresults = false;
+    monitor.stderr.on("data", async data => {
+      data = data.toString();
+      if (data.includes("subscribe\t/broadcast/0.1")) {
+        broadcast = true;
+      }
+      if (data.includes("subscribe\t/taskresults/0.1")) {
+        taskresults = true;
+      }
+      if (taskresults && broadcast) {
+        resolve();
+      }
+    });
+  });
+}, 60000);
+
+describe("start after bootstrap", function() {
+  this.timeout(60000);
+
+  it("connect to bootstrap", function() {
     const bootstrap = spawn(
       "node",
       [
@@ -55,13 +112,14 @@ test("connect to bootstrap", () => {
         "--mock-core",
         "--core",
         "127.0.0.1:3456",
-        "--ethereum-address ",
+        "--ethereum-address",
         account,
         "--ethereum-contract-address",
         enigmaContractAddress
       ],
       { cwd: "/tmp/enigma-p2p" }
     );
+    killList.push(bootstrap);
 
     const monitor = spawn("node", [
       "main.js",
@@ -72,13 +130,150 @@ test("connect to bootstrap", () => {
       "--enigma-contract-address",
       enigmaContractAddress
     ]);
+    killList.push(monitor);
 
-    monitor.stderr.on("data", async data => {
-      data = data.toString();
-      if (data.includes("peer:connect")) {
-        expect(true).toBeTruthy();
-        resolve();
-      }
+    return new Promise(resolve => {
+      monitor.stderr.on("data", async data => {
+        data = data.toString();
+        if (data.includes("peer:connect\tQmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm")) {
+          resolve();
+        }
+      });
     });
   });
-}, 60000);
+
+  it("subscribe to worker topic", function() {
+    const bootstrap = spawn(
+      "node",
+      [
+        "src/cli/cli_app.js",
+        "-i",
+        "B1",
+        "-p",
+        "B1",
+        "--auto-init",
+        "--lonely-node",
+        "--mock-core",
+        "--core",
+        "127.0.0.1:3456",
+        "--ethereum-address",
+        account,
+        "--ethereum-contract-address",
+        enigmaContractAddress
+      ],
+      { cwd: "/tmp/enigma-p2p" }
+    );
+    killList.push(bootstrap);
+
+    const monitor = spawn("node", [
+      "main.js",
+      "--bootstrap",
+      "/ip4/127.0.0.1/tcp/10300/ipfs/QmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm",
+      "--enigma-contract-json-path",
+      "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json",
+      "--enigma-contract-address",
+      enigmaContractAddress
+    ]);
+    killList.push(monitor);
+
+    return new Promise(resolve => {
+      monitor.stderr.on("data", async data => {
+        data = data.toString();
+        if (/subscribe\t[a-f0-9]{40}\b/.test(data)) {
+          resolve();
+        }
+      });
+    });
+  });
+});
+
+describe("start before bootstrap", function() {
+  this.timeout(60000);
+
+  it("connect to bootstrap", function() {
+    const monitor = spawn("node", [
+      "main.js",
+      "--bootstrap",
+      "/ip4/127.0.0.1/tcp/10300/ipfs/QmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm",
+      "--enigma-contract-json-path",
+      "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json",
+      "--enigma-contract-address",
+      enigmaContractAddress
+    ]);
+    killList.push(monitor);
+
+    const bootstrap = spawn(
+      "node",
+      [
+        "src/cli/cli_app.js",
+        "-i",
+        "B1",
+        "-p",
+        "B1",
+        "--auto-init",
+        "--mock-core",
+        "--core",
+        "127.0.0.1:3456",
+        "--ethereum-address",
+        account,
+        "--ethereum-contract-address",
+        enigmaContractAddress
+      ],
+      { cwd: "/tmp/enigma-p2p" }
+    );
+    killList.push(bootstrap);
+
+    return new Promise(resolve => {
+      monitor.stderr.on("data", async data => {
+        data = data.toString();
+        if (data.includes("peer:connect\tQmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm")) {
+          resolve();
+        }
+      });
+    });
+  });
+
+  it("subscribe to worker topic", function() {
+    const monitor = spawn("node", [
+      "main.js",
+      "--bootstrap",
+      "/ip4/127.0.0.1/tcp/10300/ipfs/QmcrQZ6RJdpYuGvZqD5QEHAv6qX4BrQLJLQPQUrTrzdcgm",
+      "--enigma-contract-json-path",
+      "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json",
+      "--enigma-contract-address",
+      enigmaContractAddress
+    ]);
+    killList.push(monitor);
+
+    const bootstrap = spawn(
+      "node",
+      [
+        "src/cli/cli_app.js",
+        "-i",
+        "B1",
+        "-p",
+        "B1",
+        "--auto-init",
+        "--lonely-node",
+        "--mock-core",
+        "--core",
+        "127.0.0.1:3456",
+        "--ethereum-address",
+        account,
+        "--ethereum-contract-address",
+        enigmaContractAddress
+      ],
+      { cwd: "/tmp/enigma-p2p" }
+    );
+    killList.push(bootstrap);
+
+    return new Promise(resolve => {
+      monitor.stderr.on("data", async data => {
+        data = data.toString();
+        if (/subscribe\t[a-f0-9]{40}\b/.test(data)) {
+          resolve();
+        }
+      });
+    });
+  });
+});
