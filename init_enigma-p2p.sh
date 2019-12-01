6da832d30a22770c7045a65be07141bc5a4714e0
assafmo <assaf.morami@gmail.com>
2019-12-01 12:23:10 +0200

    git clone -b develop enigma-p2p because https://github.com/enigmampc/enigma-p2p/pull/263

--- init_enigma-p2p.sh
+++ init_enigma-p2p.sh
@@ -7,7 +7,7 @@ if [[ -f "/tmp/enigma-p2p/test/ethereum/scripts/build/contracts/Enigma.json" ]];
 fi
 
 rm -rf /tmp/enigma-p2p
-git clone -b libp2p-encryption --single-branch https://github.com/enigmampc/enigma-p2p.git /tmp/enigma-p2p
+git clone -b develop --single-branch https://github.com/enigmampc/enigma-p2p.git /tmp/enigma-p2p
 cd /tmp/enigma-p2p
 npm install
 
